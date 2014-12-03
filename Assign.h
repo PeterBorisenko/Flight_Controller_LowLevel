212f8ba47a8ef155b7a78171afd0e1acca353701
Peter Hirt <peter@awesome39.com>
2014-12-03 23:07:30 +0200

    Defined Timer prescalers aliases. Defined ESC registers aliases.

--- Assign.h
+++ Assign.h
@@ -9,16 +9,36 @@
 #ifndef ASSIGN_H_
 #define ASSIGN_H_
 
+#include <avr/io.h>
 
+#define TMR_OFF 0b000
+#define PSC_1     0b001
+#define PSC_0_8     0b010
+#define PSC_0_64    0b011
+#define PSC_0_256   0b100
+#define PSC_0_1024  0x101
+#define EXT_0_FALLING     0x110
+#define EXT_0_RISING      0x111
+#define PSC_2_8     0b010
+#define PSC_2_32    0b011
+#define PSC_2_64   0b100
+#define PSC_2_128  0x101
+#define PSC_2_256     0x110
+#define PSC_2_1024      0x111
 // Motor (ESC) Control Pins/Ports
     // Needed Software PWM !
 
 #define ESC_port PORTD
 #define ESC_dir DDRD
-#define FL_pin PD3
-#define FR_pin PD4
-#define BL_pin PD5
-#define BR_pin PD6
+#define FL_pin PIND3
+#define FR_pin PIND4
+#define BL_pin PIND5
+#define BR_pin PIND6
+#define FL_reg OCR0A
+#define FR_reg OCR0B
+#define BL_reg OCR2A
+#define BR_reg OCR2B
+
 
 // I2C
 

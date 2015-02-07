0c015c26d8bce4b02fc27774fd3417043959d951
Peter Hirt <peter@awesome39.com>
2015-02-07 16:44:13 +0200

    New vector types

--- Assign.h
+++ Assign.h
@@ -36,7 +36,17 @@
 #define ASK_STATUS	0x32
 
 // Data format
-typedef int16_t vect_t;
+typedef struct {
+	int16_t X;
+	int16_t Y;
+	int16_t Z;
+} vect_t;
+
+typedef struct {
+	float X;
+	float Y;
+	float Z;
+} vect_float_t;
 #define DATA_WIDTH 2
 
 // Timer Prescaler
